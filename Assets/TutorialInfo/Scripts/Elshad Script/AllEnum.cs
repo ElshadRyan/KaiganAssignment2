34e34466f763ba6432c01d61ef3d3e2dba2bfffd
ElshadRyan <elshadpw@gmail.com>
2025-07-26 22:22:24 +0700

    asd

---
+++ Assets/TutorialInfo/Scripts/Elshad Script/AllEnum.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public enum _skinCategories
+{
+    head = 0, top = 1, bottom = 2, shoes = 3, accesories = 4, preset = 5
+}
---
+++ Assets/TutorialInfo/Scripts/Elshad Script/M_NPC_Parent.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class M_NPC_Parent : MonoBehaviour
+{
+    public SkinnedMeshRenderer[] F_prefabSkinnedMeshRenderers;
+    public SkinnedMeshRenderer[] M_prefabSkinnedMeshRenderers;
+
+    public virtual void ChangingSkin(Transform rootBone, SkinnedMeshRenderer currSkinnedMeshRenderers, int ID)
+    {
+
+    }
+
+    public virtual void ChangingTop()
+    {
+
+    }
+
+    public virtual void ChangingBottom()
+    {
+
+    }
+
+    public virtual void ChangingShoes()
+    {
+
+    }
+
+    public virtual void ChangingAccesories()
+    {
+
+    }
+
+    public virtual void ChangingHead()
+    {
+
+    }
+
+    public virtual void ChangingOutfitPreset()
+    {
+
+    }
+}
